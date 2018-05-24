1f8f43a18a83b653151a4433b2a82317822ed311
Claude Ayitey <clauditey@gmail.com>
2018-05-24 14:57:39 +0000

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/help'
-  get 'pages/about'
-  get 'pages/privacy'
+  get '/' => 'pages/home'
+  get '/help' => 'pages/help'
+  get '/about' => 'pages/about'
+  get '/privacy' => 'pages/privacy'
   resources :jobs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/' => 'jobs#index'
-  
 end
