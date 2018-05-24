2e516516c5e44c9f2a440357f5ff7df1a4e487f3
Claude Ayitey <clauditey@gmail.com>
2018-05-24 14:50:39 +0000

    necessary routes added, jobs will now show on home page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :jobs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/' => 'jobs#index'
+  
 end
