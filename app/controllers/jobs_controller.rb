32f26caa641794b8c35432da36074b8504105ed1
Claude Ayitey <clauditey@gmail.com>
2018-06-06 13:08:53 +0000

    archived field was causing errors on production when jobs is created. Commented now.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -90,6 +90,6 @@ class JobsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def job_params
-      params.require(:job).permit(:role, :duration, :salary, :requirements, :qualification, :perks, :company, :contact_email, :poster_name, :poster_email, :phone, :user_id, :archived)
+      params.require(:job).permit(:role, :duration, :salary, :requirements, :qualification, :perks, :company, :contact_email, :poster_name, :poster_email, :phone, :user_id)
     end
 end
