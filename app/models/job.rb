1c8efb9f94572c2cdb66eefa7d00df36810fbb6e
Claude Ayitey <clauditey@gmail.com>
2018-06-15 21:19:38 +0000

    Combo Update Here
    I changed the content in the other static pages; privacy, about and help pages with some minimal but relevant information
    Disabled the twitter gem so i could work on it better later
    Made some style changes to navbar to make border bottom color match link text color
    Activated the archive checkbox in the job edit form
    Users can now archive job posts
    	Archived job posts will not show to anyone except the owner of the job post
    	When they show to the owners, they have a different background to make them stand out from other active jobs
    Made some page changes to the header area to allow for share buttons
    	Added tweet link which shares page link to Twitter
    	Added share link which shares page link to Facebook
    Added some helper methods to check ownership for job posts
    Tried adding a patch method to toggle archiving from the job list view, couldn't get that to work.
    	Related code for that exists but has been commented out

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,11 @@
 class Job < ApplicationRecord
     belongs_to :user
+
+    def is_archived?
+        if self.archived == true
+            return true
+        else
+            return false
+        end        
+    end
 end
---
+++ config/initializers/twitter.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+
+# client = Twitter::REST::Client.new do |config|
+#   config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+#   config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+#   config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+#   config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
+# end
