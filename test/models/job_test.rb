6eab16ec51b4bb7d81a82156a67fe17cd2d68e5a
Yaw Boakye <wheresyaw@gmail.com>
2018-10-11 14:19:55 +0200

    annotate the models

--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: jobs
+#
+#  id            :bigint(8)        not null, primary key
+#  role          :string
+#  duration      :string
+#  salary        :string
+#  requirements  :string
+#  qualification :string
+#  perks         :string
+#  company       :string
+#  contact_email :string
+#  phone         :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  user_id       :integer
+#  archived      :boolean          default(FALSE)
+#
+
 require 'test_helper'
 
 class JobTest < ActiveSupport::TestCase
