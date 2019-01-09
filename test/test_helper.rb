16be8a300ad345217366d9c5d00cc502baa28b93
Yaw Boakye <wheresyaw@gmail.com>
2019-01-09 20:19:01 +0100

    install capybara

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,9 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+require 'capybara/rails'
+require 'capybara/minitest'
+
 class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
   include FactoryBot::Syntax::Methods
@@ -12,5 +15,15 @@ class ActiveSupport::TestCase
       with.test_framework :minitest
       with.library :rails
     end
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  include Capybara::DSL
+  include Capybara::Minitest::Assertions
+
+  def teardown
+    Capybara.reset_session!
+    Capybara.use_default_driver
   end
 end
