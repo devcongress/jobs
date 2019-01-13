4aa855e59fd25f718da876e06b4ad20b288f05c5
Yaw Boakye <wheresyaw@gmail.com>
2019-01-13 14:33:34 +0100

    add system test for creating company

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,7 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  include Devise::Test::IntegrationHelpers
+
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,6 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
-require 'capybara/rails'
-require 'capybara/minitest'
-
 class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
   include FactoryBot::Syntax::Methods
@@ -15,15 +12,5 @@ class ActiveSupport::TestCase
       with.test_framework :minitest
       with.library :rails
     end
-  end
-end
-
-class ActionDispatch::IntegrationTest
-  include Capybara::DSL
-  include Capybara::Minitest::Assertions
-
-  def teardown
-    Capybara.reset_session!
-    Capybara.use_default_driver
   end
 end
