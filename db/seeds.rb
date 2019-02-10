1095882cd9fd52ea8ad34cb56d85a3c4bedaceb4
Andrew Smith <a.smith@silentworks.co.uk>
2019-02-10 12:48:12 +0000

    Add seed information database

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,126 @@
 
 open("https://gist.githubusercontent.com/claudey/679c682e4291d7b54059c7c19b1dfdf2/raw/3687da0264ca4fa6867d12b561c164a8b9c29af6/industries.txt", "r") do |content|
   content.each_line do |industry_name|
-    Industry.create name: industry_name
+    Industry.where(name: industry_name)
+      .first_or_create.update_attributes({name: industry_name})
   end
+end
+
+# Add a default test user
+User.where(email: "test@example.com")
+  .first_or_create.update_attributes({ 
+    email: 'test@example.com', 
+    password: 'password1', 
+    password_confirmation: 'password1' 
+  })
+
+companies = [
+  {
+    name: 'Acme Inc', 
+    industry: 'Entertainment', 
+    description: 'We are leaders in creating fun stuff.',
+    website: 'https://www.acmeinc.example.com/',
+    email: 'info@acmeinc.example.com',
+    phone: '012345657',
+    city: 'Lego',
+    state_or_region: 'Alb',
+    post_code: '12029',
+    country: 'Somewhere'
+  },
+  {
+    name: 'Brick House', 
+    industry: 'Media', 
+    description: 'We build playable bricks.',
+    website: 'https://brickhouse.example.com/',
+    email: 'info@brickhouse.example.com',
+    phone: '01234687',
+    city: 'Lego',
+    state_or_region: 'Alb',
+    post_code: '12022',
+    country: 'Somewhere'
+  },
+  {
+    name: 'Cool Tech', 
+    industry: 'Education', 
+    description: 'We are leaders cool technology.',
+    website: 'https://cooltech.example.com/',
+    email: 'info@cooltech.example.com',
+    phone: '01234565789',
+    city: 'Lego',
+    state_or_region: 'Alb',
+    post_code: '12029',
+    country: 'Somewhere'
+  }
+]
+
+user = User.find_by(email: "test@example.com")
+
+# Add some default companies and add relationship to test user
+companies.each do |company|
+  puts company[:email]
+  Company.where(email: company[:email])
+    .first_or_create.update_attributes(company)
+  
+  c = Company.find_by(email: company[:email])
+  unless user.companies.exists?(c.id)
+    user.companies << c
+  end
+end
+
+jobs = [
+  {
+    role: 'Software Engineer',
+    duration: 'Full-Time',
+    salary: 'USD 500 - 1k',
+    requirements: 'Some stuff',
+    qualification: '*Have a diploma*',
+    perks: '- Free lunch',
+    city: 'Port 1',
+    country: 'Fya',
+    company_id: 1,
+    apply_link: 'https://jobs.devcongress.org/jobs/44-software-developer-petra-trust',
+  },
+  {
+    role: 'Senior Software Engineer',
+    duration: 'Full-Time',
+    salary: 'USD 2 - 3k',
+    requirements: 'Some stuff',
+    qualification: '*Have a degree*',
+    perks: '- Free lunch',
+    city: 'Port 1',
+    country: 'Fya',
+    company_id: 1,
+    apply_link: 'https://jobs.devcongress.org/jobs/44-software-developer-petra-trust',
+  },
+  {
+    role: 'User Experience Designer',
+    duration: 'Full-Time',
+    salary: 'USD 1 - 2k',
+    requirements: 'Good eyes for design',
+    qualification: '*Have existing experience*',
+    perks: '- Free lunch',
+    city: 'Port 44',
+    country: 'Fya',
+    company_id: 2,
+    apply_link: 'https://jobs.devcongress.org/jobs/44-software-developer-petra-trust',
+  },
+  {
+    role: 'Technical Writer',
+    duration: 'Full-Time',
+    salary: 'USD 500 - 1k',
+    requirements: 'Some stuff',
+    qualification: '*Have a diploma*',
+    perks: '- Free lunch',
+    remote_ok: 'True',
+    city: 'Port 24',
+    country: 'Fya',
+    company_id: 3,
+    apply_link: 'https://jobs.devcongress.org/jobs/44-software-developer-petra-trust',
+  },
+]
+
+jobs.each do |job|
+  puts job[:role]
+  Job.where(role: job[:role], requirements: job[:requirements])
+    .first_or_create.update_attributes(job)
 end
