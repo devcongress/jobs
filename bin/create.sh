5546272ad8cc1e8c2cf15a09517400b50d6cf920
Stefan Froelich <stefan.froelich@andela.com>
2019-02-11 01:37:18 +0100

    add script to setup docker-compose environment

---
+++ bin/create.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "creating containers"
+docker-compose up --build -d
+
+echo "waiting for containers to start"
+# inelegant but efficient. we wait a few seconds
+secs=5
+while [ $secs -gt 0 ]; do
+   echo -ne "$secs\033[0K\r"
+   sleep 1
+   : $((secs--))
+done
+
+echo "running migrations and seeding database"
+docker-compose run --rm web rake db:migrate db:seed
+
+echo "complete!!!"
