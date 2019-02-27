0a645b06de7fe6856e4db9111611a4827bd4f33a
Yaw Boakye <wheresyaw@gmail.com>
2019-02-27 14:35:17 +0100

    flying blind trying to fix devise

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,7 @@ Rails.application.routes.draw do
     get    'login',  to: 'devise/sessions#new',     as: :new_user_session
     post   'login',  to: 'devise/sessions#create',  as: :user_session
     delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
+
     # registrations
     put    '/account',  to: 'devise/registrations#update'
     delete '/account',  to: 'devise/registrations#destroy'
@@ -32,7 +33,8 @@ Rails.application.routes.draw do
     get    '/register', to: 'devise/registrations#new',    as: :new_user_registration
     get    '/account',  to: 'devise/registrations#edit',   as: :edit_user_registration
     patch  '/account',  to: 'devise/registrations#update', as: :user_registration
-    get    '/account/cancel', to: 'devise/registrations#cancel', as: :cancel_user_registration# passwords 
+    get    '/account/cancel', to: 'devise/registrations#cancel', as: :cancel_user_registration
+
     # passwords
     get   'new-pass',  to: 'devise/passwords#new',    as: :new_user_password
     get   'edit-pass', to: 'devise/passwords#edit',   as: :edit_user_password
