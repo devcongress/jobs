aa4ea0815a5994b6213758e3cd3b32d20c1770d1
Yaw Boakye <wheresyaw@gmail.com>
2019-03-01 21:08:51 +0100

    show time since user's account was created

--- test/controllers/profile_controller_test.rb
+++ test/controllers/profile_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class ProfileControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
   include ERB::Util
+  include ActionView::Helpers::DateHelper
 
   setup do
     @user = FactoryBot.create(:user)
@@ -25,6 +26,7 @@ class ProfileControllerTest < ActionDispatch::IntegrationTest
     assert_response :ok
 
     assert_match new_company_path, @response.body # link to register company
+    assert_match time_ago_in_words(@user.created_at), @response.body
     @user.companies.each do |c|
       # the profile page includes a list of all companies
       # and the number of jobs posted for each.
