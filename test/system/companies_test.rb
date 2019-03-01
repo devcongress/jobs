b0fee5eb994aa36dc4e7b3edf6eda8c3cdb39223
Yaw Boakye <wheresyaw@gmail.com>
2019-03-01 23:22:52 +0100

    add views for editing companies

--- test/system/companies_test.rb
+++ test/system/companies_test.rb
@@ -32,4 +32,24 @@ class CompaniesTest < ApplicationSystemTestCase
     assert_text company.city
     assert_text company.country
   end
+
+  test "editing a company" do
+    company = FactoryBot.create(:company)
+    company_params = FactoryBot.attributes_for(:company)
+    @user.companies << company
+
+    visit edit_company_url(company)
+
+    fill_in "Name",          with: company_params[:name]
+    fill_in "About",         with: company_params[:description]
+    fill_in "Website",       with: company_params[:website]
+    fill_in "Contact Email", with: company_params[:email]
+    fill_in "Contact Phone", with: company_params[:phone]
+    fill_in "City",          with: company_params[:city]
+    fill_in "State/Region",  with: company_params[:state_or_region]
+    fill_in "Post/Zip Code", with: company_params[:post_code]
+    fill_in "Country",       with: company_params[:country]
+
+    click_on "Save"
+  end
 end
