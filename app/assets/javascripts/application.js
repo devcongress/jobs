5c3f0ca2d8c93c8242eabed87de4cefac8e43dfd
Claude Ayitey <clauditey@gmail.com>
2019-03-12 00:07:00 +0000

    added UIKit sass files.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,11 +15,3 @@
 //= require turbolinks
 //= require trix
 //= require_tree .
-
-// var archiveSwitch = new Boolean(false);
-
-// $(".job--list__archive-link").on("click", function() {
-//     archiveSwitch = !archiveSwitch;
-//     var temp_ = tempSwitch == true ? "Unarchive" : "Archive";
-//     $(".job--list__archive-link").html(temp_);
-// });
