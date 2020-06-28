2f3a9f825e7bacb74bc201a60cb8e98e414630e4
Nana Adjei Manu <claeusdev@gmail.com>
2020-06-28 10:12:01 +0000

    refactor variables and add jobs to profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,5 +2,7 @@ class ProfileController < ApplicationController
   before_action :authenticate_user!
 
   def show
+    @companies = current_user.companies
+    @jobs = @companies.map {|company| company.jobs }
   end
 end
